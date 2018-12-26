3abf3c3f938cc7b021589fa00201961d5d9d3868
lyl <254364502@qq.com>
2018-12-26 16:25:12 +0800

    侧边 form

--- src/view/components/tables/tables.vue
+++ src/view/components/tables/tables.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
     <Card>
+      <Button @click="showWindowBDrawer = true" type="primary" style="float: right;">添加</Button>
       <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns" @on-delete="handleDelete"/>
       <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button>
+      <drag-drawer v-model="showWindowBDrawer"
+                   :width.sync="width1"
+                   :min-width="600"
+                   :placement="placementComputed"
+                   :draggable="draggable"
+                   :scrollable="true">
+        <div slot="header">
+          <Icon type="md-aperture" :size="18"></Icon>
+          <b>这是标题</b>
+        </div>
+        <test></test>
+      </drag-drawer>
     </Card>
   </div>
 </template>
 
 <script>
 import Tables from '_c/tables'
+import DragDrawer from '_c/drag-drawer'
+import test from '@/components/form/test'
+
 import { getTableData } from '@/api/data'
 export default {
   name: 'tables_page',
   components: {
-    Tables
+    Tables,
+    DragDrawer,
+    test
   },
   data () {
     return {
+      /* 侧边弹出 */
+      showWindowBDrawer: false,
+      /* 侧边弹出 */
+      draggable: false,
+      /* 侧边弹出 */
+      placement: false,
+      /* 侧边弹出 */
+      width1: 600,
       columns: [
         { title: 'Name', key: 'name', sortable: true },
         { title: 'Email', key: 'email', editable: true },
@@ -49,6 +75,12 @@ export default {
     }
   },
   methods: {
+    /* 侧边弹出 */
+    handleResize (event) {
+      const { atMin } = event
+      /* eslint-disable */
+      console.log(atMin);
+    },
     handleDelete (params) {
       console.log(params)
     },
@@ -58,6 +90,12 @@ export default {
       })
     }
   },
+  /* 侧边弹出 */
+  computed: {
+    placementComputed () {
+      return this.placement ? 'left' : 'right'
+    }
+  },
   mounted () {
     getTableData().then(res => {
       this.tableData = res.data.returnData
@@ -65,7 +103,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
